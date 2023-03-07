221d627c2662dc3d1462c92ea220abc47dce0265
kurmanjan2506 <kurmanjan25nurbekova@gmail.com>
2023-03-07 16:27:11 +0600

    new ulr in cors

--- listApi/settings.py
+++ listApi/settings.py
@@ -193,7 +193,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     'http://3.83.158.158',
-    'https://booking-rooms.vercel.app/',
+    'https://booking-rooms.vercel.app',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
