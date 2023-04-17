df01af434acc637b5e696b8e2b2ebc52be187061
kurmanjan2506 <kurmanjan25nurbekova@gmail.com>
2023-04-17 16:59:27 +0600

    fix bag

--- room/signals.py
+++ room/signals.py
@@ -1,26 +1,26 @@
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-from .models import BookedRoom, Notification
-from celery import shared_task
-
-
-@shared_task()
-def task(created:bool, instance):
-    if created:
-        Notification.objects.create(
-            message=f'Состоится встреча {instance.get("title")}\n'
-                    f'Чтобы посмотреть детали встречи, перейдите на нее'
-        )
-    else:
-        Notification.objects.create(
-            message=f'Состоится встреча {instance.get("title")}\n'
-                    f'Чтобы посмотреть детали встречи, перейдите на нее'
-        )
-
-@receiver(post_save, sender=BookedRoom)
-def post_save_meet(created, **kwargs):
-    instance = kwargs.get("instance")
-    instance = {
-        "title": instance.title,
-    }
-    task.delay(True if created else False, instance)
+# from django.dispatch import receiver
+# from django.db.models.signals import post_save
+# from .models import BookedRoom, Notification
+# from celery import shared_task
+#
+#
+# @shared_task()
+# def task(created:bool, instance):
+#     if created:
+#         Notification.objects.create(
+#             message=f'Состоится встреча {instance.get("title")}\n'
+#                     f'Чтобы посмотреть детали встречи, перейдите на нее'
+#         )
+#     else:
+#         Notification.objects.create(
+#             message=f'Состоится встреча {instance.get("title")}\n'
+#                     f'Чтобы посмотреть детали встречи, перейдите на нее'
+#         )
+#
+# @receiver(post_save, sender=BookedRoom)
+# def post_save_meet(created, **kwargs):
+#     instance = kwargs.get("instance")
+#     instance = {
+#         "title": instance.title,
+#     }
+#     task.delay(True if created else False, instance)
